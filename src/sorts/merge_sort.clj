fbc1ea48721b3c20f7bb978b549dc06a9b01322c
fr33m0nk <prashantsinha@protonmail.com>
2023-07-08 13:43:24 +0530

    Adds Lazy merge sort

--- src/sorts/merge_sort.clj
+++ src/sorts/merge_sort.clj
@@ -18,4 +18,16 @@
     (let [midpoint (/ (count collection) 2)
           [left right] (split-at midpoint collection)]
       (mrg (merge-sort left) (merge-sort right)))))
-
+(defn lazy-merge-sort
+  [int-seq]
+  (letfn [(merge*
+            [[f1 & r1 :as l1] [f2 & r2 :as l2]]
+            (cond
+              (nil? f1) l2
+              (nil? f2) l1
+              (<= f1 f2) (lazy-seq (cons f1 (merge* r1 l2)))
+              (> f1 f2) (lazy-seq (cons f2 (merge* l1 r2)))))]
+    (if (next int-seq)
+      (let [[left right] (split-at (/ (count int-seq) 2) int-seq)]
+        (merge* (lazy-merge-sort left) (lazy-merge-sort right)))
+      int-seq)))
--- src/sorts/quick_sort.clj
+++ src/sorts/quick_sort.clj
@@ -1,12 +1,12 @@
 (ns sorts.quick-sort)
 
-(defn lazy-quicksort [[head & tail]]
+(defn lazy-quick-sort [[head & tail]]
   (when head
-    (lazy-cat (lazy-quicksort (filter #(< % head) tail))
+    (lazy-cat (lazy-quick-sort (filter #(< % head) tail))
               [head]
-              (lazy-quicksort (remove #(< % head) tail)))))
+              (lazy-quick-sort (remove #(< % head) tail)))))
 
-(defn quicksort
+(defn quick-sort
   [int-seq]
   (loop [done []
          [part & parts] (list int-seq)]
